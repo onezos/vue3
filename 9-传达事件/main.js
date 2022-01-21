829052e6babc39a4d412c1f98a550b29b3be8d06
onezos <gdd9399@gmail.com>
2022-01-21 19:21:19 +0800

    添加第九课 传达事件

---
+++ 9-传达事件/main.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            cart: [],
+            premium: false
+        }
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        },
+        removeById(id) {
+            const index = this.cart.indexOf(id)
+            if (index > -1) {
+               this.cart.splice(index, 1)
+            }
+        }
+    }
+})
